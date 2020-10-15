fa5b9a1be3696ecd403932d36cadeb65cd537ad2
AlberHerTol <alberto.herrero.toledano@alumnos.uva.es>
2020-10-15 15:58:07 +0200

    Formulario Login Completo

--- lib/Formulario.dart
+++ lib/Formulario.dart
@@ -17,18 +17,27 @@ class FormularioState extends State<Formulario>{
 
  */
 
-class Formulario extends StatefulWidget{
+class Login extends StatefulWidget{
   @override
-  FormularioState createState(){
+  LoginState createState(){
     print("CreateState de Formulario");
-    return new FormularioState();
+    return new LoginState();
   }
 }
 
-class FormularioState extends State<Formulario>{
+class LoginState extends State<Login>{
 
   bool _mostrar= false;
+  bool _acceder = false;
   Icon iconoMostrar= Icon(Icons.visibility);
+
+
+  clickAcceder(){
+    print('Click acceder');
+    setState(() {
+      _acceder= !_acceder;
+    });
+  }
   clickVer(){
     print('Click ver');
     setState(() {
@@ -42,27 +51,69 @@ class FormularioState extends State<Formulario>{
   @override
   Widget build (BuildContext context){
     return
-      Row(
-        children:[
-          Expanded(child:
-            TextField(
-              obscureText: !_mostrar,    //_ hace la vble privada
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+
+      Container(
+          margin: const EdgeInsets.only(left:10.0, top: 100.0, right: 10.0, bottom:350.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:[
+              Row(children:[
+                Expanded(child:
+                TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                  ),
+                ),
+                ),
+              ]
+
               ),
-            ),
-          ),
-          IconButton(
-            icon: iconoMostrar,
-            onPressed: (){
-              clickVer();
-            }
-          )
+              Row(
+                children:[
+                  Expanded(child:
+                  TextField(
+                    obscureText: !_mostrar,    //_ hace la vble privada
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                  ),
+                  IconButton(
+                      icon: iconoMostrar,
+                      onPressed: (){
+                        clickVer();
+                      }
+                  )
 
+                ]
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    Container(
+                        padding: EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.blue,
+                        ),
+                        child:
+                        FlatButton(
+                          onPressed: (){clickAcceder();},
+                          child: Text('Acceder'),
+                        )
+                    )
+                  ]
+              )
         ]
-      )
 
-     ;
+        )
+      );
   }
 }
